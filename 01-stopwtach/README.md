f84b0a736a4999b1cbaabbbfffd0e9a87b79eaea
Zach Sievers <Zach.Sievers@dolphin-corp.com>
2024-04-09 15:21:48 -0400

    add 01-stopwatch

---
+++ 01-stopwtach/README.md
@@ -0,0 +1,5 @@
+Step 1: 
+    - create react app w/ the name of your project
+    - cd into folder
+    - delete everything you don't need
+Step 2: Lets Gooooo
---
+++ 01-stopwtach/src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import { useEffect, useState } from 'react'
+
+
+function App() {
+const [time, setTime] = useState(0)
+const [running, setRunning] = useState(false)
+
+useEffect(() => {
+  let interval;
+  if (running) {
+    interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 10)
+    }, 10)
+  } else if (!running) {
+    clearInterval(interval);
+  }
+  return () => clearInterval(interval);
+}, [running])
+  return (
+    <div className='flex flex-col items-center justify-center py-8'>
+      <h1 className='text-2lg font-semibold pb-2'>01-Stopwatch</h1>
+      <div className='text-xl font-semibold py-4'>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+      <div className='w-1/3 max-w-md flex flex-row justify-evenly'>
+        {running ? (
+        <button className='border rounded-lg py-1 px-3.5'
+        onClick={() => { setRunning(false) }}>Stop</button>)
+        : (
+        <button className='border rounded-lg py-1 px-3'
+        onClick={() => { setRunning(true) }}>Start</button>)
+        }
+        <button className='border rounded-lg py-1 px-2.5'
+        onClick={() => { setTime(0) }}>Reset</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
